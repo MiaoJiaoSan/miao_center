95383d72c6623f815947356a6b577b3f7d20397a
MiaoJiaoSan <lpfnwndproc@163.com>
2020-04-26 00:06:00 +0800

    eureka

---
+++ duplicate/src/main/java/com/miaojiaosan/DuplicateApplication.java
@@ -0,0 +1,20 @@
+package com.miaojiaosan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author miaojiaosan
+ * @date: 2020/4/25
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DuplicateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DuplicateApplication.class,args);
+  }
+}
+
---
+++ eureka/src/main/java/com/miaojiaosan/WebConfiguration.java
@@ -0,0 +1,21 @@
+package com.miaojiaosan;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author miaojiaosan
+ * @date: 2020/4/25
+ */
+@SpringBootConfiguration
+@EnableWebSecurity
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    //使用Security需要关闭csrf
+    http.csrf().disable();
+    super.configure(http);
+  }
+}
